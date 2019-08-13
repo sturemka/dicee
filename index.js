995859c079f775e70524075871c49df21cb78eeb
Seweryn Turemka <s.turemka@gmail.com>
2019-08-13 22:41:26 +0200

    create dicee game

---
+++ index.js
@@ -0,0 +1,27 @@
+// Random number 1-6 for player 1
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+// Select and set image for player 1
+var randomImageSource = "images/dice" + randomNumber1 + ".png"; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
+// Random number 1-6 for player 2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+// Select and set image for player 2
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
